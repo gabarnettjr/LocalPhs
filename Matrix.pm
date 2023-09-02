6aebbe18b616085fc470648f13214ca507644d7c
gabarnettjr <gabarnettjr@gmail.com>
2023-09-02 10:57:05 -0600

    Update Matrix.pm

--- Matrix.pm
+++ Matrix.pm
@@ -11,6 +11,12 @@ use warnings;
 
 ################################################################################
 
+# CONSTANTS
+
+sub pi {return 3.141592652589793; }
+
+################################################################################
+
 # CONSTRUCTORS
 
 sub new {
@@ -22,7 +28,7 @@ sub new {
         $numRows = scalar @{$items};
         $numCols = scalar @{@{$items}[0]};
     } else {
-        die "Bad input for new matrix.  Give ref to array.";
+        die "Bad input for new matrix.  Give ref to array.    ";
     }
     
     my $self = ["Matrix", $items, $numRows, $numCols];
@@ -34,7 +40,7 @@ sub new {
 
 sub zeros {
     # New matrix of zeros.
-    die "Matrix::zeros() requires two inputs, numRows and numCols.    " if scalar @_ != 2;
+    die "Matrix::zeros() requires two inputs: numRows and numCols.    " if scalar @_ != 2;
     my ($numRows, $numCols) = @_;
     
     my @items = ();
@@ -56,7 +62,7 @@ sub zeros {
 
 sub ones {
     # New matrix of ones.
-    die "Matrix::ones() requires two inputs, numRows and numCols.    " if scalar @_ != 2;
+    die "Matrix::ones() requires two inputs: numRows and numCols.    " if scalar @_ != 2;
     my ($numRows, $numCols) = @_;
     
     my @items = ();
@@ -76,9 +82,31 @@ sub ones {
 
 
 
+sub rand {
+    # New matrix whose values are randomly chosen from between 0 and 1.
+    die "Matrix::rand() requires two inputs: numRows and numCols.    " if scalar @_ != 2;
+    my ($numRows, $numCols) = @_;
+    
+    my @items = ();
+    
+    for (my $i = 0; $i < $numRows; $i++) {
+        my @tmp = ();
+        for (my $j = 0; $j < $numCols; $j++) {
+            push(@tmp, rand);
+        }
+        push(@items, \@tmp);
+    }
+    
+    my $self = ["Matrix", \@items, $numRows, $numCols];
+    bless $self;
+    return $self;
+}
+
+
+
 sub eye {
     # New Identity matrix.
-    die "Matrix::eye() requires one input, numRows (numCols always equals numRows).    " if scalar @_ != 1;
+    die "Matrix::eye() requires one input: numRows (numCols always equals numRows).    " if scalar @_ != 1;
     my ($numRows) = @_;
 
     my $numCols = $numRows;
@@ -167,15 +195,25 @@ sub numCols {
 
 ################################################################################
 
+sub len {
+    die "No extra input required.    " if scalar @_ != 1;
+    my $self = shift;
+    return $self->numRows if $self->numCols() == 1;
+    return $self->numCols if $self->numRows() == 1;
+    die "Length is only well-defined for a 1D matrix.    ";
+}
+
+
+
 sub meshgrid {
     my $x = shift;
     my $y = shift;
     
-    my $xx = Matrix::zeros($y->numCols(), $x->numCols());
-    my $yy = Matrix::zeros($y->numCols(), $x->numCols());
+    my $xx = Matrix::zeros($y->numCols, $x->numCols);
+    my $yy = Matrix::zeros($y->numCols, $x->numCols);
     
-    for (my $i = 0; $i < $y->numCols(); $i++) {
-        for (my $j = 0; $j < $x->numCols(); $j++) {
+    for (my $i = 0; $i < $y->numCols; $i++) {
+        for (my $j = 0; $j < $x->numCols; $j++) {
             $xx->set($i, $j, $x->item($j));
             $yy->set($i, $j, $y->item($i));
         }
@@ -190,19 +228,19 @@ sub vstack {
     my $self = shift;
     my $other = shift;
     
-    die "Dimension mismatch.    " if $self->numCols() != $other->numCols();
+    die "Dimension mismatch.    " if $self->numCols() != $other->numCols;
     
-    my $out = Matrix::zeros($self->numRows() + $other->numRows(), $other->numCols());
+    my $out = Matrix::zeros($self->numRows() + $other->numRows, $other->numCols);
     
-    for (my $i = 0; $i < $self->numRows(); $i++) {
-        for (my $j = 0; $j < $self->numCols(); $j++) {
+    for (my $i = 0; $i < $self->numRows; $i++) {
+        for (my $j = 0; $j < $self->numCols; $j++) {
             $out->set($i, $j, $self->item($i, $j));
         }
     }
     
-    for (my $i = $self->numRows(); $i < $self->numRows() + $other->numRows(); $i++) {
-        for (my $j = 0; $j < $self->numCols(); $j++) {
-            $out->set($i, $j, $other->item($i - $self->numRows(), $j));
+    for (my $i = $self->numRows; $i < $self->numRows() + $other->numRows; $i++) {
+        for (my $j = 0; $j < $self->numCols; $j++) {
+            $out->set($i, $j, $other->item($i - $self->numRows, $j));
         }
     }
     
@@ -217,11 +255,11 @@ sub hstack {
     
     my $out;
     eval {
-        $out = $self->transpose()->vstack($other->transpose());
+        $out = $self->transpose->vstack($other->transpose);
     };
     die if $@;
     
-    return $out->transpose();
+    return $out->transpose;
 }
 
 
@@ -229,11 +267,11 @@ sub hstack {
 sub flatten {
     my $self = shift;
     
-    my $out = Matrix::zeros(1, $self->numRows() * $self->numCols());
+    my $out = Matrix::zeros(1, $self->numRows() * $self->numCols);
     my $k = 0;
     
-    for (my $i = 0; $i < $self->numRows(); $i++) {
-        for (my $j = 0; $j < $self->numCols(); $j++) {
+    for (my $i = 0; $i < $self->numRows; $i++) {
+        for (my $j = 0; $j < $self->numCols; $j++) {
             $out->set($k, $self->item($i, $j));
             $k++;
         }
@@ -247,7 +285,7 @@ sub flatten {
 sub disp {
     my $self = shift;
     
-    foreach my $row (@{$self->items()}) {
+    foreach my $row (@{$self->items}) {
         foreach my $item (@{$row}) {
             printf "%10.6f ", $item;
         }
@@ -266,12 +304,12 @@ sub item {
         $i = shift;
         $j = shift;
         return @{@{$self->items()}[$i]}[$j];
-    } elsif (scalar @_ == 1 && $self->numRows() == 1) {
+    } elsif (scalar @_ == 1 && $self->numRows == 1) {
         $j = shift;
-        return @{@{$self->items()}[0]}[$j];
-    } elsif (scalar @_ == 1 && $self->numCols() == 1) {
+        return @{@{$self->items}[0]}[$j];
+    } elsif (scalar @_ == 1 && $self->numCols == 1) {
         $i = shift;
-        return @{@{$self->items()}[$i]}[0];
+        return @{@{$self->items}[$i]}[0];
     }
 }
 
@@ -281,7 +319,7 @@ sub row {
     my $self = shift;
     my $i = shift;
     
-    my $row = @{$self->items()}[$i];
+    my $row = @{$self->items}[$i];
     my $out = Matrix::new([$row]);
     
     return $out;
@@ -307,15 +345,15 @@ sub set {
         $i = shift;
         $j = shift;
         $val = shift;
-        @{@{$self->items()}[$i]}[$j] = $val;
-    } elsif (scalar @_ == 2 && $self->numRows() == 1) {
+        @{@{$self->items}[$i]}[$j] = $val;
+    } elsif (scalar @_ == 2 && $self->numRows == 1) {
         $j = shift;
         $val = shift;
-        @{@{$self->items()}[0]}[$j] = $val;
-    } elsif (scalar @_ == 2 && $self->numCols() == 1) {
+        @{@{$self->items}[0]}[$j] = $val;
+    } elsif (scalar @_ == 2 && $self->numCols == 1) {
         $i = shift;
         $val = shift;
-        @{@{$self->items()}[$i]}[0] = $val;
+        @{@{$self->items}[$i]}[0] = $val;
     } else {
         print STDERR "\nInputs not understood.\n";  die;
     }
@@ -326,9 +364,9 @@ sub set {
 sub copy {
     my $self = shift;
     
-    my $out = Matrix::zeros($self->numRows(), $self->numCols());
+    my $out = Matrix::zeros($self->numRows, $self->numCols);
     
-    for (my $i = 0; $i < $out->numRows(); $i++) {
+    for (my $i = 0; $i < $out->numRows; $i++) {
         for (my $j = 0; $j < $out->numCols; $j++) {
             $out->set($i, $j, $self->item($i, $j));
         }
@@ -345,10 +383,10 @@ sub plus {
     my $self = shift;
     my $other = shift;
     
-    my $numRows = $self->numRows();
-    my $numCols = $self->numCols();
+    my $numRows = $self->numRows;
+    my $numCols = $self->numCols;
     
-    if (ref $other && ($numRows != $other->numRows() || $numCols != $other->numCols())) {
+    if (ref $other && ($numRows != $other->numRows || $numCols != $other->numCols)) {
         print STDERR "\nMatrices must be the same size to add them together.\n";  die;
     }
     
@@ -387,12 +425,12 @@ sub dotProduct {
         print STDERR "\nInput must be a 1D matrix.\n";  die;
     }
     
-    my $numRows = $self->numRows();
-    my $numCols = $self->numCols();
+    my $numRows = $self->numRows;
+    my $numCols = $self->numCols;
 
     my $dotProduct = 0;
     
-    if ($numRows != $other->numRows() || $numCols != $other->numCols()) {
+    if ($numRows != $other->numRows || $numCols != $other->numCols) {
         print STDERR "\nMatrices must be the same size to dot them.\n";  die;
     } elsif ($numRows != 1 && $numCols != 1) {
         print STDERR "\nThis function is only implemented for 1D matrices.\n";  die;
@@ -414,10 +452,10 @@ sub dotProduct {
 sub transpose {
     my $self = shift;
     
-    my $out = Matrix::zeros($self->numCols(), $self->numRows());
+    my $out = Matrix::zeros($self->numCols, $self->numRows);
     
-    for (my $i = 0; $i < $out->numRows(); $i++) {
-        for (my $j = 0; $j < $out->numCols(); $j++) {
+    for (my $i = 0; $i < $out->numRows; $i++) {
+        for (my $j = 0; $j < $out->numCols; $j++) {
             $out->set($i, $j, $self->item($j, $i));
         }
     }
@@ -436,31 +474,31 @@ sub dot {
     
     if (ref $other) {
         
-        if ($self->numCols() != $other->numRows()) {
-            print "\n\$numCols of first matrix (" . $self->numCols() . ") must equal \$numRows of second (" . $other->numRows() . ").\n";
+        if ($self->numCols != $other->numRows) {
+            print "\n\$numCols of first matrix (" . $self->numCols . ") must equal \$numRows of second (" . $other->numRows . ").\n";
             print "\$first = \n";
-            $self->disp();
+            $self->disp;
             print "\n";
             print "\$second = \n";
-            $other->disp();
+            $other->disp;
             die;
         }
         
-        $prod = Matrix::zeros($self->numRows(), $other->numCols());
-        $other = $other->transpose();
+        $prod = Matrix::zeros($self->numRows, $other->numCols);
+        $other = $other->transpose;
         
-        for (my $i = 0; $i < $prod->numRows(); $i++) {
-            for (my $j = 0; $j < $prod->numCols(); $j++) {
+        for (my $i = 0; $i < $prod->numRows; $i++) {
+            for (my $j = 0; $j < $prod->numCols; $j++) {
                 $prod->set($i, $j, $self->row($i)->dotProduct($other->row($j)));
             }
         }
         
     } else {
         
-        $prod = Matrix::zeros($self->numRows(), $self->numCols());
+        $prod = Matrix::zeros($self->numRows, $self->numCols);
         
-        for (my $i = 0; $i < $self->numRows(); $i++) {
-            for (my $j = 0; $j < $self->numCols(); $j++) {
+        for (my $i = 0; $i < $self->numRows; $i++) {
+            for (my $j = 0; $j < $self->numCols; $j++) {
                 $prod->set($i, $j, $self->item($i, $j) * $other);
             }
         }
@@ -475,12 +513,12 @@ sub dotTimes {
     my $self = shift;
     my $other = shift;
     
-    my $numRows = $self->numRows();
-    my $numCols = $self->numCols();
+    my $numRows = $self->numRows;
+    my $numCols = $self->numCols;
     
     if (! ref $other) {
         print STDERR "\nUse dot() to multiply a scalar by a matrix.\n";  die;
-    } elsif ($numRows != $other->numRows() || $numCols != $other->numCols()) {
+    } elsif ($numRows != $other->numRows || $numCols != $other->numCols) {
         print STDERR "\nMatrices must be the same size to (dot) multiply them together.\n";  die;
     }
     
@@ -503,8 +541,8 @@ sub pow {
     
     my $out = $self->copy();
     
-    for (my $i = 0; $i < $self->numRows(); $i++) {
-        for (my $j = 0; $j < $self->numCols(); $j++) {
+    for (my $i = 0; $i < $self->numRows; $i++) {
+        for (my $j = 0; $j < $self->numCols; $j++) {
             $out->set($i, $j, $self->item($i, $j) ** $pow);
         }
     }
@@ -516,19 +554,19 @@ sub pow {
 
 sub norm {
     my $self = shift;
-    die "Only 1D matrices are supported right now.    " if $self->numRows() > 1 && $self->numCols() > 1;
+    die "Only 1D matrices are supported right now.    " if $self->numRows > 1 && $self->numCols > 1;
     my $p = shift;
 
     $p = 2 if ! $p;
     
     my $ell;
-    $ell = $self->numRows() if $self->numCols() == 1;
-    $ell = $self->numCols() if $self->numRows() == 1;
+    $ell = $self->numRows if $self->numCols == 1;
+    $ell = $self->numCols if $self->numRows == 1;
 
     my $norm = 0;
 
     for (my $j = 0; $j < $ell; $j++) {
-        $norm += ($self->item($j) ** $p);
+        $norm += (abs $self->item($j)) ** $p;
     }
 
     return $norm ** (1 / $p);
@@ -541,9 +579,9 @@ sub swapRows {
     my $i = shift;
     my $j = shift;
     
-    my $tmp = @{$self->items()}[$i];
-    @{$self->items()}[$i] = @{$self->items()}[$j];
-    @{$self->items()}[$j] = $tmp;
+    my $tmp = @{$self->items}[$i];
+    @{$self->items}[$i] = @{$self->items}[$j];
+    @{$self->items}[$j] = $tmp;
 }
 
 
@@ -552,17 +590,17 @@ sub solve {
     my $self = shift;
     my $rhs = shift;
     
-    my $A = $self->copy();
-    my $b = $rhs->copy();
+    my $A = $self->copy;
+    my $b = $rhs->copy;
 
-    if ($A->numRows() != $A->numCols() || $A->numRows() != $b->numRows()) {
+    if ($A->numRows() != $A->numCols || $A->numRows != $b->numRows) {
         print "Rows(A) = " . $A->numRows . "\n";
         print "Rows(b) = " . $b->numRows . "\n";
         print STDERR "\nA should be square.  Rows(A) should equal Rows(b).\n";  die;
     }
     
-    my $nRows = $A->numRows();
-    my $nCols = $A->numCols();
+    my $nRows = $A->numRows;
+    my $nCols = $A->numCols;
 
     # Apply row operations to transform A to upper triangular form.
     for (my $j = 0; $j < $nCols - 1; $j++) {
@@ -585,7 +623,7 @@ sub solve {
             for (my $k = $j; $k < $nCols; $k++) {
                 $A->set($i, $k, $A->item($i, $k) + $factor * $A->item($j, $k));
             }
-            for (my $ell = 0; $ell < $b->numCols(); $ell++) {
+            for (my $ell = 0; $ell < $b->numCols; $ell++) {
                 $b->set($i, $ell, $b->item($i, $ell) + $factor * $b->item($j, $ell));
             }
         }
@@ -593,13 +631,13 @@ sub solve {
 
     # Use back substitution to finish solving for @x.
     my $x = $b;
-    for (my $ell = 0; $ell < $b->numCols(); $ell++) {
+    for (my $ell = 0; $ell < $b->numCols; $ell++) {
         $x->set($nRows-1, $ell, $b->item($nRows-1, $ell) / $A->item($nRows-1, $nRows-1));
     }
     my $k = $nRows;
     for (my $i = $nRows - 2; $i >= 0; $i--) {
         $k--;
-        for (my $ell = 0; $ell < $b->numCols(); $ell++) {
+        for (my $ell = 0; $ell < $b->numCols; $ell++) {
             my $dot = 0;
             for (my $j = $k; $j < $nRows; $j++) {
                 $dot += ($A->item($i, $j) * $x->item($j, $ell));
