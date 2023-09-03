ab44e9ecf8a00d190612e5688958658dde679256
gabarnettjr <gabarnettjr@gmail.com>
2023-09-02 20:10:47 -0600

    Update LocalPhs.pm

--- LocalPhs.pm
+++ LocalPhs.pm
@@ -80,34 +80,75 @@ sub vals {
 
 ################################################################################
 
+# OLD SLOW METHOD
+
+# sub splines {
+    # my $self = shift;
+    # my $splines = @{$self}[6];
+    # return $splines if defined $splines && ! scalar @_;
+    # return @{$splines}[shift] if defined $splines;
+
+    # $splines = [];
+    # my ($stencil, $vals, $j, $k, $diff);
+
+    # for ($j = 0; $j < $self->nodes->numRows; $j++) {
+        # $stencil = Matrix::zeros(1, $self->nodes->numCols);
+        # $vals = $self->vals->row($j);
+        # for ($k = 0; $k < $self->nodes->numRows; $k++) {
+            # $diff = $self->nodes->row($k)->minus($self->nodes->row($j));
+            # if ($k != $j && $diff->norm < $self->stencilRadius) {
+                # $stencil = $stencil->vstack($diff);
+                # $vals = $vals->vstack($self->vals->row($k));
+            # }
+        # }
+        # my $phs = Phs::new($self->rbfExponent, $self->polyDegree, $stencil, $vals);
+        # push(@{$splines}, $phs);
+        # print "Stencil-size = " . $stencil->numRows() . "\n";
+    # }
+    
+    # @{$self}[6] = $splines;
+    # return $splines if ! scalar @_;
+    # return @{$splines}[shift];
+    # die "Failed to get polyharmonic spline(s).";
+# }
+
+################################################################################
+
+# NEW FAST METHOD
+
 sub splines {
     my $self = shift;
+    my $ind = shift;
     my $splines = @{$self}[6];
-    return $splines if defined $splines && ! scalar @_;
-    return @{$splines}[shift] if defined $splines;
-
-    $splines = [];
-    my ($stencil, $vals, $j, $k, $diff);
-
-    for ($j = 0; $j < $self->nodes->numRows; $j++) {
-        $stencil = Matrix::zeros(1, $self->nodes->numCols);
-        $vals = $self->vals->row($j);
-        for ($k = 0; $k < $self->nodes->numRows; $k++) {
-            $diff = $self->nodes->row($k)->minus($self->nodes->row($j));
-            if ($k != $j && $diff->norm() < $self->stencilRadius) {
-                $stencil = $stencil->vstack($diff);
-                $vals = $vals->vstack($self->vals->row($k));
-            }
+    if (! defined $splines) {
+        $splines = [];
+        for (my $i = 0; $i < $self->nodes->numRows; $i++) {
+            push(@{$splines}, 0);
         }
-        my $phs = Phs::new($self->rbfExponent, $self->polyDegree, $stencil, $vals);
-        push(@{$splines}, $phs);
-        print "Stencil-size = " . $stencil->numRows() . "\n";
+    }
+    return @{$splines}[$ind] if @{$splines}[$ind];
+
+    my ($stencil, $vals, $k, $diff);
+
+    $stencil = Matrix::zeros(1, $self->nodes->numCols);
+    $vals = $self->vals->row($ind);
+    
+    for ($k = 0; $k < $self->nodes->numRows; $k++) {
+        $diff = $self->nodes->row($k)->minus($self->nodes->row($ind));
+        if ($k != $ind && $diff->norm < $self->stencilRadius) {
+            $stencil = $stencil->vstack($diff);
+            $vals = $vals->vstack($self->vals->row($k));
+        }
     }
     
+    my $phs = Phs::new($self->rbfExponent, $self->polyDegree, $stencil, $vals);
+    @{$splines}[$ind] = $phs;
+    
+    print "Stencil-size = " . $stencil->numRows . "\n";
+    
     @{$self}[6] = $splines;
-    return $splines if ! scalar @_;
-    return @{$splines}[shift];
-    die "Failed to get polyharmonic spline(s).";
+    return $phs;
+    die "Failed to get polyharmonic spline.";
 }
 
 ################################################################################
@@ -123,9 +164,11 @@ sub evaluate {
     my $self = shift;
     my $evalPts = shift;
     
-    my ($min, $ind, $i, $j, $point, $node, $dist, $out);
+    my ($min, $ind, $i, $j, $point, $node, $dist, $out, $time);
     
     $out = Matrix::zeros($evalPts->numRows, 1);
+    
+    $time = time;
     
     for ($i = 0; $i < $evalPts->numRows; $i++) {
         $point = $evalPts->row($i);
@@ -140,6 +183,10 @@ sub evaluate {
         }
         $out->set($i, $self->splines($ind)->evaluate($point->minus($self->nodes->row($ind)))->item(0, 0));
     }
+    
+    $time = time - $time;
+    
+    print "To evaluate relevant splines takes $time seconds.\n";
     
     return $out;
 }
