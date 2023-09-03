16abd4965aeddf4c7cb4242a9de0d3c21d3ef475
gabarnettjr <gabarnettjr@gmail.com>
2023-09-03 12:25:53 -0600

    Update Phs.jl

--- julia/Phs.jl
+++ julia/Phs.jl
@@ -1,9 +1,10 @@
 # using LinearAlgebra
 
-mutable struct Phs {
+mutable struct Phs
     rbfExponent::UInt
     polyDegree::UInt
-    nodes::Float64{2}
-    vals::Float64{1}
+    nodes::Array{Float64,2}
+    vals::Array{Float64,1}
 end
 
+phs = Phs(3, 1, [1 2; 3 4; 5 6], [1;2;3])
