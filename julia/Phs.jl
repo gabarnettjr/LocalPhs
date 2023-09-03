43d872b5d8fa8444672aaf6d21e21e574e5b67e5
gabarnettjr <gabarnettjr@gmail.com>
2023-09-03 12:35:26 -0600

    Update Phs.jl

--- julia/Phs.jl
+++ julia/Phs.jl
@@ -1,10 +1,22 @@
 # using LinearAlgebra
 
 mutable struct Phs
+    dims::UInt
     rbfExponent::UInt
     polyDegree::UInt
     nodes::Array{Float64,2}
     vals::Array{Float64,1}
+    coeffs::Array{Float64,1}
 end
 
-phs = Phs(3, 1, [1 2; 3 4; 5 6], [1;2;3])
+# function Phs_coeffs(phs::Phs)
+    # A = Phs_phi(Phs_r(Phs.nodes)
+# end
+
+dims = 2
+rbfExponent = 3
+polyDegree = 1
+nodes = [1 2; 3 4; 5 6]
+vals = [1; 2; 3]
+
+phs = Phs(dims, rbfExponent, polyDegree, nodes, vals)
