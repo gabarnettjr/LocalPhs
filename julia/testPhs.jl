473737364488790549a015a9ac4a91e39d38579d
gabarnettjr <gabarnettjr@gmail.com>
2023-09-03 19:36:52 -0600

    Update testPhs.jl

--- julia/testPhs.jl
+++ julia/testPhs.jl
@@ -1,62 +1,54 @@
 
 include("Phs.jl")
 
-dims = 2
-rbfExponent = 3;
-polyDegree = 1;
+rbfExponent = 3
+polyDegree = 1
 
 # Define spatial domain [a,b] x [c,d], with m rows and n columns.
 a = -1;  b = 1;  n = 6
 c = -1;  d = 1;  m = 6
 
 # Define number of rows (M) and columns (N) for evaluation points.
-M = 10;
-N = 10;
+M = 10
+N = 10
 
 ################################################################################
 
-x = range(a, stop = b, steps = n)
-y = range(c, stop = d, steps = m)
-xx = []
-yy = []
-k = 1
-for i = 1 : m
-    for j = 1 : n
-        xx = vcat(xx, x[j])
-        yy = vcat(yy, y[i])
+function  makeNodes(x, y)
+    n = length(x)
+    m = length(y)
+    xx = zeros(m * n, 1)
+    yy = zeros(m * n, 1)
+    k = 1
+    for i = 1 : m
+        for j = 1 : n
+            xx[k] = x[j]
+            yy[k] = y[i]
+            k += 1
+        end
     end
+    nodes = hcat(xx, yy)
+    return nodes
 end
-nodes = hcat(xx, yy)
+
+x = range(a, stop = b, length = n)
+y = range(c, stop = d, length = m)
+nodes = makeNodes(x, y)
 zz = Phs_testFunc2d(nodes)
 
-X = range(a, stop = b, steps = M)
-Y = range(c, stop = d, steps = M)
-XX = []
-YY = []
-k = 1
-for i = 1 : M
-    for j = 1 : N
-        XX = vcat(XX, X[j])
-        YY = vcat(Y, Y[i])
-    end
-end
-NODES = hcat(XX, YY)
+X = range(a, stop = b, length = N)
+Y = range(c, stop = d, length = M)
+NODES = makeNodes(X, Y)
 ZZ = Phs_testFunc2d(NODES)
 
-spline = Phs();
-spline.dims = size(nodes, 2)
-spline.rbfExponent = rbfExponent
-spline.polyDegree = polyDegree
-spline.nodes = nodes
-
+spline = Phs_new(rbfExponent, polyDegree, nodes, zz)
 estimate = Phs_evaluate(spline, NODES)
 
-print("estimate = ";
-print(transpose(estimate))
-print("\n")
-print("ZZ = \n")
-print(transpose(ZZ))
-print("\n")
+println("estimate = ")
+println(transpose(estimate))
+println("ZZ = ")
+println(transpose(ZZ))
+println()
 diff = estimate - ZZ
-print("diff = \n")
-print(transpose(diff))
+println("diff = ")
+println(transpose(diff))
