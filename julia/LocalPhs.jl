12d2bc94b44e59f0fb44cd9d621b6906961e4201
gabarnettjr <gabarnettjr@gmail.com>
2023-09-03 19:36:23 -0600

    Create LocalPhs.jl

---
+++ julia/LocalPhs.jl
@@ -0,0 +1,92 @@
+
+include("Phs.jl")
+
+################################################################################
+
+mutable struct LocalPhs
+    dims::Int64
+
+    rbfExponent::Int64
+    polyDegree::Int64
+    nodes::Matrix{Float64}
+    vals::Matrix{Float64}
+    stencilRadius::Float64
+
+    splines::Array{Phs}
+end
+
+################################################################################
+
+function LocalPhs_new(rbfExponent::Int64, polyDegree::Int64, nodes::Matrix{Float64}, vals::Matrix{Float64}, stencilRadius::Float64)
+    dims = size(nodes, 2)
+    phs = Phs_new(rbfExponent, polyDegree, zeros(1, 1), zeros(1, 1))
+    splines = fill(phs, size(nodes, 1))
+    self = LocalPhs(dims, rbfExponent, polyDegree, nodes, vals, stencilRadius, splines)
+    return self
+end
+
+################################################################################
+
+function LocalPhs_splines(self::LocalPhs, ind::Int64)
+    if ! self.splines[ind].isNull
+        println("re-using a spline.")
+        return self.splines[ind]
+    end
+
+    diff = zeros(1, self.dims)
+    stencil = zeros(1, self.dims)
+    val = zeros(1, 1)
+    vals = zeros(1, 1)
+    vals[1,:] = self.vals[ind, :]
+    
+    for k = 1 : size(self.nodes, 1)
+        diff[1,:] = self.nodes[k, :] - self.nodes[ind, :]
+        val[1,:] = self.vals[k,:]
+        if (k != ind) && (norm(diff) < self.stencilRadius)
+            stencil = vcat(stencil, diff)
+            vals = vcat(vals, val)
+        end
+    end
+    
+    phs = Phs_new(self.rbfExponent, self.polyDegree, stencil, vals)
+    self.splines[ind] = phs
+    
+    println("Stencil-size = ", size(stencil, 1))
+    
+    return phs
+end
+
+################################################################################
+
+function LocalPhs_evaluate(self::LocalPhs, evalPts::Matrix{Float64})
+    ind = 0
+    tmp = zeros(1, self.dims)
+    out = zeros(size(evalPts, 1), 1)
+
+    t = time()
+    
+    for i = 1 : size(evalPts, 1)
+        point = evalPts[i, :]
+        min = 99
+        for j = 1 : size(self.nodes, 1)
+            node = self.nodes[j, :]
+            dist = norm(point - node)
+            if (dist < min)
+                min = dist
+                ind = j
+            end
+        end
+        tmp[1, :] = point - self.nodes[ind, :]
+        out[i] = Phs_evaluate(LocalPhs_splines(self, ind), tmp)[1,1]
+    end
+    
+    t = time() - t
+    
+    println("To evaluate relevant splines took ", t, " seconds.")
+    
+    return out
+end
+
+################################################################################
+
+
