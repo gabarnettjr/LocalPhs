6d33ee715590dd0f8e4f363d35bcca0686aaaa1f
gabarnettjr <gabarnettjr@gmail.com>
2023-09-03 19:37:25 -0600

    Create testLocalPhs.jl

---
+++ julia/testLocalPhs.jl
@@ -0,0 +1,31 @@
+
+using LinearAlgebra
+
+include("LocalPhs.jl")
+
+################################################################################
+
+dims = 3                                                            # dimensions
+rbfExponent = 3                      # odd number exponent to use in the phs rbf
+polyDegree = 1                          # maximum polynomial degree in the basis
+a = -1;  b = 1                                            # bounds on the domain
+n = 1000                                                       # number of nodes
+N = 100                                            # number of evaluation points
+stencilRadius = 4/8         # how far away to look for neighbors of a given node
+
+################################################################################
+
+nodes = rand(n, dims)
+nodes = nodes * (b - a) .+ a
+zz = Phs_testFunc2d(nodes)
+
+NODES = rand(N, dims)
+NODES = NODES * (b - a) .+ a
+ZZ = Phs_testFunc2d(NODES)
+
+phs = LocalPhs_new(rbfExponent, polyDegree, nodes, zz, stencilRadius)
+estimate = LocalPhs_evaluate(phs, NODES)
+
+diff = estimate - ZZ
+print("average error = ", norm(diff, 1) / length(diff), "\n")
+
