77063cf1d2ffd376250577d48072877a6e4e39c6
gabarnettjr <gabarnettjr@gmail.com>
2023-09-19 20:32:26 -0600

    Update EstimateNbaWinPct.pl

--- julia/EstimateNbaWinPct.pl
+++ julia/EstimateNbaWinPct.pl
@@ -3,8 +3,13 @@
 use strict;
 use warnings;
 
+my $case = $ARGV[0];
+
 # Get the needed data from the raw NBA text files.
-system "perl wrangleNbaData.pl";
+# case 1: field goal pct, defensive rebounds, turnovers.
+# case 2: points, defensive rebounds, opponent points.
+# case 3: points, opponent points.
+system "perl wrangleNbaData.pl $case";
 
 # Interpolate the known data to estimate the win pct of the current teams.
 system "julia Interpolate.jl data/nodes.txt data/values.txt data/evalPts.txt data/estimates.txt";
@@ -25,13 +30,28 @@ foreach my $line (@nba) {
 }
 
 # Get the array of estimated win percentages for each team.
-(open EST, "data/estimates.txt") || die "Faile to open file for reading.";
+(open EST, "<data/estimates.txt") || die "Failed to open file for reading.";
 my @est = <EST>;  close EST;
 my @wpc = ();
 foreach my $line (@est) {
     chomp $line;
     if ($line =~ /\S+/) {
         push @wpc, $line;
+    }
+}
+
+# Get the evalPts info to be displayed at the end.
+(open EVAL, "<data/evalPts.txt") || die "Failed to open file for reading.";
+my @evalPts = <EVAL>;  close EVAL;
+my (@pts, @drb, @tov, @pto, @fgp);
+@pts = @drb = @tov = @pto = @fgp = ();
+foreach my $line (@evalPts) {
+    chomp $line;
+    my @line = split /\s+/, $line;
+    if ($case == 2) {
+        push @pts, $line[0];
+        push @drb, $line[1];
+        push @pto, $line[2];
     }
 }
 
@@ -42,14 +62,25 @@ if (scalar @teams != scalar @wpc || scalar @teams != scalar @trueWpc) {
     die "Number of teams does not match number of win percentages.";
 }
 
-# Save a new text file that shows both, and print it to standard output.
+# Save a new text file that shows a summary, and print it to standard output.
 (open SUM, ">data/nbaWpcEstimates.txt") || die "Failed to open file for writing.";
-printf SUM "%-25s %9s %6s    %4s\n", "Team Name", "Estimated", "Actual", "Diff";
-printf     "%-25s %9s %6s    %4s\n", "Team Name", "Estimated", "Actual", "Diff";
-for (my $i = 0; $i < scalar @teams; $i++) {
-    my $diff = $wpc[$i] - $trueWpc[$i];
-    printf SUM "%-25s %-9.3f %-6.3f   %6.3f\n", $teams[$i], $wpc[$i], $trueWpc[$i], $diff;
-    printf     "%-25s %-9.3f %-6.3f   %6.3f\n", $teams[$i], $wpc[$i], $trueWpc[$i], $diff;
+if ($case == 2) {
+    printf SUM "%-25s %-5s  %-4s  %-9s %9s %6s    %4s\n", "Team Name", "Pts", "Drb", "Opp", "Estimated", "Actual", "Diff";
+    printf     "%-25s %-5s  %-4s  %-9s %9s %6s    %4s\n", "Team Name", "Pts", "Drb", "Opp", "Estimated", "Actual", "Diff";
+    for (my $i = 0; $i < scalar @teams; $i++) {
+        my $diff = $wpc[$i] - $trueWpc[$i];
+        printf SUM "%-25s %5.1f  %4.1f  %-9.1f %-9.3f %-6.3f   %6.3f\n", $teams[$i], $pts[$i], $drb[$i], $pto[$i], $wpc[$i], $trueWpc[$i], $diff;
+        printf     "%-25s %5.1f  %4.1f  %-9.1f %-9.3f %-6.3f   %6.3f\n", $teams[$i], $pts[$i], $drb[$i], $pto[$i], $wpc[$i], $trueWpc[$i], $diff;
+    }
+} else {
+    printf SUM "%-25s %9s %6s    %4s\n", "Team Name", "Estimated", "Actual", "Diff";
+    printf     "%-25s %9s %6s    %4s\n", "Team Name", "Estimated", "Actual", "Diff";
+    for (my $i = 0; $i < scalar @teams; $i++) {
+        my $diff = $wpc[$i] - $trueWpc[$i];
+        printf SUM "%-25s %-9.3f %-6.3f   %6.3f\n", $teams[$i], $wpc[$i], $trueWpc[$i], $diff;
+        printf     "%-25s %-9.3f %-6.3f   %6.3f\n", $teams[$i], $wpc[$i], $trueWpc[$i], $diff;
+    }
 }
+
 close SUM;
 
